249b81d9bda843283d91f9de80e2c6026ac316e8
ca1997mo <acsuarezg@unal.edu.co>
2026-02-04 22:15:56 -0500

    Intento de limpieza

---
+++ 01_code/02_clean_data.r
@@ -0,0 +1,36 @@
+## Cargue de la información previamente descargada ##
+
+base <- readRDS("~/BDML-PS08/01_code/data_output/01_data_scrapping_web_page.rds")
+colnames(base)
+
+library(dplyr)
+library(tidyverse)
+
+base_filtrada<-base%>%filter(age>=18 & ocu==1 & !is.na(y_total_m) & !is.na(sex) & !is.na(age))%>%
+  select(age,sex,y_total_m,totalHoursWorked,relab,mes,clase,college,fex_c,informal,maxEducLevel,
+         pet)
+
+## salario minimo en 2018 781.242
+## linea pobreza bogotá 434630, en hogares
+
+### miramos posible estacionalidad
+# al observar los meses no se encuentra estacionalidad
+
+base_filtrada%>%group_by(mes)%>%summarise(mediana=median(y_total_m),promedio=mean(y_total_m),maximo=max(y_total_m),minimo=min(y_total_m))
+
+
+### miramos los cuantiles
+# valores muy altos arriba
+quantile(base_filtrada$y_total_m, c(.001,.9,.95,.99,.995,.999), na.rm=TRUE)
+
+# quiénes son, si los quitamos quedaría fuera una proporción similar de hombres y mujeres
+base_filtrada%>%filter(y_total_m>=quantile(y_total_m,.99))%>%group_by(sex)%>%summarise(n())
+base_filtrada%>%filter(y_total_m>=quantile(y_total_m,.001))%>%group_by(sex)%>%summarise(n())
+
+## base depurada
+
+base_depurada<-base_filtrada%>%filter(y_total_m<=quantile(y_total_m,.99)&y_total_m>=quantile(y_total_m,.001))
+
+hist(base_depurada$y_total_m)
+
+summary(base_depurada)
