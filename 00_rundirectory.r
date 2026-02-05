8881114b63be3c966a1ac9dd5892814b43f196f0
SanyLeon <100888886+SanyLeon@users.noreply.github.com>
2026-02-05 18:52:57 -0500

    ...

---
+++ 00_rundirectory.r
@@ -0,0 +1,36 @@
+##########################################################
+# Master script
+#
+# Running this file reproduces all results in the repository.
+#
+# To reproduce all results, run:
+# from an interactive R session: source("01_code/00_rundirectory.R")   
+# or from the command line: R CMD BATCH 01_code/00_rundirectory.R
+#
+# Authors:
+# - Sany León Curasi
+# - Andrés Camilo Suárez
+# - Juan Rueda
+##########################################################
+
+# Step 1: Download and construct the raw dataset
+rm(list = ls())
+if (require("pacman") == F){install.packages("pacman")}else{require("pacman")}
+
+pacman::p_load(tidyverse)
+# run code
+list = c(# 01_importar
+  "01_code/01_data_scraper.R"
+)
+
+run = walk(.x = list, .f = function(x){
+  print(paste0("Running: ", x))
+  Sys.sleep(3)
+  source(x)}
+)
+
+
+# Step 2: Filters data 
+# The filtered dataset is saved to a temporary folder for downstream use
+
+#source("01_code/02_clean_data.R")
